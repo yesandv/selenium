4102433ab0b258b05f577b2def56dec37d124257
yesandv <grc@protonmail.com>
2022-02-12 18:22:51 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -2,12 +2,17 @@ import json
 
 import pytest
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options as FFOptions
 
 
 @pytest.fixture(scope="session")
 def browser(config_as_json):
     if config_as_json.get("browser") == "Firefox":
         browser = webdriver.Firefox()
+    elif config_as_json.get("browser") == "Nightly":
+        options = FFOptions()
+        options.binary_location = '/Applications/Firefox Nightly.app/Contents/MacOS/firefox-bin'
+        browser = webdriver.Firefox(firefox_options=options, executable_path='/usr/local/bin/geckodriver')
     elif config_as_json.get("browser") == "Safari":
         browser = webdriver.Safari()
     else:
