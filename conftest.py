6d51791a3dc78aae1ed9adbd6f4d0ffeb60b31c2
yesandv <grc@protonmail.com>
2022-02-12 17:18:28 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,10 +1,24 @@
+import json
+
 import pytest
 from selenium import webdriver
 
 
 @pytest.fixture(scope="session")
-def browser():
-    browser = webdriver.Chrome()
+def browser(config_as_json):
+    if config_as_json.get("browser") == "Firefox":
+        browser = webdriver.Firefox()
+    elif config_as_json.get("browser") == "Safari":
+        browser = webdriver.Safari()
+    else:
+        browser = webdriver.Chrome()
     browser.implicitly_wait(3)
     yield browser
     browser.quit()
+
+
+@pytest.fixture(scope="session")
+def config_as_json():
+    with open("config.json") as config_as_json:
+        config = json.load(config_as_json)
+        return config
