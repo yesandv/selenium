46e4f7769fb1d6884d29c0d497e74aab15bc18db
yesandv <grc@protonmail.com>
2022-02-27 16:31:28 +0300

    Update the test

--- tests/test_check_admin_menu.py
+++ tests/test_check_admin_menu.py
@@ -1,7 +1,7 @@
-from forms.sign_in_form import SignInForm
+from forms.admin_form import AdminForm
 
 
 def test_verify_admin_menu_items(browser):
-    admin_auth_form = SignInForm(browser)
-    admin_console = admin_auth_form.sign_in(username="admin", password="admin", remember_me=True)
-    admin_console.verify_menu_items_headers()
+    admin_form = AdminForm(browser)
+    admin_form.sign_in(username="admin", password="admin", remember_me=True)
+    admin_form.verify_menu_items_headers()
